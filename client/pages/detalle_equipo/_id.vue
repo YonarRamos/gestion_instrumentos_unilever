53e68a8a62ccf23f1487646660ba79abf02e7920
unknown <yonar.ramos@systelec.com>
2021-03-09 18:18:19 -0300

    Fnk Btn

--- client/pages/detalle_equipo/_id.vue
+++ client/pages/detalle_equipo/_id.vue
@@ -428,14 +428,11 @@
                 <v-row>      
                   <v-col cols="12" sm="6">
                     <v-row>
-                      <v-col cols="6" class="pr-0 pl-4">
-                        <div class="overline">
+                      <v-col class="d-flex pr-1">
+                        <div class="overline" style="padding-top:8px;">
                           Tareas de Calibracion
                         </div>
-                      </v-col>
-                      <v-spacer></v-spacer>
-                      <v-col cols="6" class="d-flex justify-start pt-2 px-0">
-                        <agregar-calibracion :instrumento="item" @click="getEquipo"/>
+                        <agregar-calibracion :instrumento="item" @click="getEquipo" class="ml-3"/>
                       </v-col>
                     </v-row>
                       <v-divider></v-divider>
@@ -453,7 +450,7 @@
                             </v-data-table> 
                         </v-card>     
                   </v-col>
-                  <v-col cols="12" sm="6">  
+                  <v-col cols="12" sm="6" class="mt-2">  
                     <div class="overline">
                       Tareas de Calibración Realizadas
                     </div>
